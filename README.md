daf6c058406e0fdfeb96117ee0814f718fecc245
孤之界 <f20001012@gmail.com>
2023-02-21 16:43:26 +0800

    add: 自動去背 add: requirements.txt misc: 修改 README.md

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # anime-face-detector
 
-使用 https://github.com/hysts/anime-face-detector 製作
+使用 [anime-face-detector](https://github.com/hysts/anime-face-detector) 以及 [anime-segmentation](https://github.com/SkyTNT/anime-segmentation) 製作，需要安裝 [PyTorch](https://pytorch.org/get-started/locally/)
 
-需要根據[原文的說明](https://github.com/hysts/anime-face-detector#installation)來安裝虛擬環境及[PyTorch](https://pytorch.org/get-started/locally/)等必要套件
+使用 venv 建立虛擬環境後執行以下指令來安裝必要套件
+
+```
+pip install -r requirements.txt
+min install -r mim-requirements.txt
+```
 
 # 開發環境
 
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
 import os
+import torch
 
 from anime_face_detector import create_detector
+from train import AnimeSegmentation
+from inference import get_mask
 
 detector = create_detector('yolov3')
 
@@ -25,6 +28,7 @@ def main():
 
 # https://www.askpython.com/python/examples/python-directory-listing
 def _list_full_paths(directory):
+    """取得目錄下所有的資料夾並返回完整路徑"""
     list = []
     for root, dirs, files in os.walk(directory):
         for f in files:
@@ -34,7 +38,42 @@ def _list_full_paths(directory):
     return list
 
 
+# https://stackoverflow.com/a/44659589
+def _image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    """使用 OpenCV 來縮放圖片"""
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
 def _detect_face(file_path: str):
+    """檢測臉部區塊並進行裁切及去背"""
     label_name = os.path.basename(os.path.dirname(file_path))
     file_name, file_extension = os.path.splitext(file_path)
     file_name = os.path.basename(file_name)
@@ -65,6 +104,7 @@ def _detect_face(file_path: str):
         box, score = box[:4], box[4]
         box = np.round(box).astype(int)
 
+        # 縮放75%以獲得更多細節
         # https://zhuanlan.zhihu.com/p/44179128
         x = box[0]
         y = box[1]
@@ -76,9 +116,6 @@ def _detect_face(file_path: str):
         x1 = cx + int(0.75 * w)
         y0 = cy - int(0.75 * h)
         y1 = cy + int(0.75 * h)
-
-        y0 = y0 - cy // 5
-        y1 = y1 - cy // 5
 
         if x0 < 0:
             x0 = 0
@@ -99,16 +136,33 @@ def _detect_face(file_path: str):
             y1 = y1 - h // 2 + w // 2
             h = w
 
+        # 忽略低於0.7分的臉
         print(f'box: {box}, score: {score}')
         if score < 0.7:
             continue
 
-        #res = image[max(0, box[1]): min(image.shape[0],  box[3]),
+        # res = image[max(0, box[1]): min(image.shape[0],  box[3]),
         #            max(0, box[0]): min(image.shape[1],  box[2])]
         res = image[y0: y0 + h, x0: x0 + w, :]
-        res = cv2.resize(res, (256, 256))
-        cv2.imencode(file_extension, res)[1].tofile(f'output\\{label_name}\\{file_name}_{i}{file_extension}')
+        res = _image_resize(res, 256)
+
+        # 移除背景
+        print('get mask')
+        res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+        mask = get_mask(model, res, use_amp=True, s=256)
+        res = np.concatenate(
+            (mask * res + 1 - mask, mask * 255), axis=2).astype(np.uint8)
+        res = cv2.cvtColor(res, cv2.COLOR_RGBA2BGRA)
+
+        cv2.imencode(file_extension, res)[1].tofile(
+            f'output\\{label_name}\\{file_name}_{i}{file_extension}')
 
 
 if __name__ == '__main__':
+    device = torch.device('cuda:0')
+    model = AnimeSegmentation.try_load(
+        'isnet_is', 'saved_models/isnetis.ckpt', 'cuda:0')
+    model.eval()
+    model.to(device)
+
     main()
